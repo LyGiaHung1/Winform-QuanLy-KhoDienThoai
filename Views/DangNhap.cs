f2750454e150591b9347e989565d2b0db41bb78e
hoangphucloc <hoangloc0706@gmail.com>
2024-10-14 15:21:00 +0700

    test committttt

--- Views/DangNhap.cs
+++ Views/DangNhap.cs
@@ -23,5 +23,10 @@ namespace PhoneWarehouse.Views
             hp.Show();
             this.Hide();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
